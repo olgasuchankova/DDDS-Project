f9920401c0366395a0a293a3212f9c6b28d6a8c2
lourdesreyes17 <lourdes.reyes@duke.edu>
2021-04-29 14:35:01 -0400

    features update

--- features.py
+++ features.py
@@ -7,13 +7,13 @@ Created on Mon Apr 19 22:57:53 2021
 extract features from each data set
 """
 import numpy as np
-from scipy.stats import mode
+from statistics import mode
 
 def get_time_features(self):
     mean = []
     minimum = []
     maximum = []
-    median = []
+    median_ft = []
     mode_ft = []
     
     for k in range(int(len(self)/15)):
@@ -21,17 +21,17 @@ def get_time_features(self):
         stop = (k*15)+16
         for j in range(6):
             mean.append(np.mean(self[start:stop,j]))
-            # minimum.append(np.min(self[start:stop,j]))
-            # maximum.append(np.max(self[start:stop,j]))
-            # median.append(np.median(self[start:stop,j]))            
-            # mode_ft.append(mode(self[start:stop,j]))
+            minimum.append(np.min(self[start:stop,j]))
+            maximum.append(np.max(self[start:stop,j]))
+            median_ft.append(np.median(self[start:stop,j]))            
+            mode_ft.append(mode(self[start:stop,j]))
     
-    np.reshape(mean,(k+1,j+1))
-    print(mean)
-    # print(minimum)
-    # print(maximum)
-    # print(median)
-    # print(mode)
-    # time_stats = np.c_[mean, minimum, maximum, median, mode_ft]
+    mean = np.reshape(mean,(k+1,j+1))
+    minimum = np.reshape(minimum,(k+1,j+1))
+    maximum = np.reshape(maximum,(k+1,j+1))
+    median_ft = np.reshape(median_ft,(k+1,j+1))
+    mode_ft = np.reshape(mode_ft,(k+1,j+1))
+    time_stats = np.c_[mean, minimum, maximum, median_ft, mode_ft]
+    # print(np.shape(time_stats))
     
-    return mean
+    return time_stats
