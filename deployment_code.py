cc7102164fa6e6a873307170809a09f03029da70
lourdesreyes17 <lourdes.reyes@duke.edu>
2021-04-29 15:10:01 -0400

    Update deployment_code.py

--- deployment_code.py
+++ deployment_code.py
@@ -7,6 +7,8 @@ Created on Mon Apr 19 23:30:39 2021
 import numpy as np
 import csv
 from features import get_time_features as gtf
+from sklearn import train_test_split
+import classifier as clfr
 
 file = open('practicedata.csv')
 numpy_array = np.loadtxt(file,delimiter=',')
@@ -14,7 +16,9 @@ numpy_array = np.loadtxt(file,delimiter=',')
 time_features = []
 time_features = gtf(numpy_array)
 
-
+labels=[]
+X_train, X_test, y_train, y_test = train_test_split(
+    time_features, labels, test_size=0.15, shuffle=False)
 # with open('practicedata.csv','r') as file:
 #     reader = csv.reader(file,quoting=csv.QUOTE_NONNUMERIC)
 #     for row in reader:
