2718d312aef61343133b44155f32a4c924f5498a
Olga Suchankova <os33@duke.edu>
2021-04-29 23:35:06 -0400

    labels & split

--- deployment_code.py
+++ deployment_code.py
@@ -9,15 +9,19 @@ import csv
 from features import get_time_features as gtf
 from sklearn.model_selection import train_test_split
 import classifier as clfr
+from data_compilation import features_loaded
 
 file = open('practicedata.csv')
 numpy_array = np.loadtxt(file,delimiter=',')
 
+load_preprocessed_data = features_loaded()
+
 time_features = []
-time_features = gtf(numpy_array)
+num_time_feat = 4
+time_features = load_preprocessed_data[:, (time_features.shape[1] - num_time_feat):]
 
 labels=[]
-labels = np.ones(len(time_features))
+labels = load_preprocessed_data[0]
 X_train, X_test, y_train, y_test = train_test_split(
     time_features, labels, test_size=0.15, shuffle=False)
 
